6508823c70bc47b483314d4c6024998cd6ce260f
inoas <mail@inoas.com>
2022-05-15 05:02:45 +0200

    styling

--- lib/meetup_web/live/game.ex
+++ lib/meetup_web/live/game.ex
@@ -33,14 +33,14 @@ defmodule MeetupWeb.Game do
     ~H"""
     <div class="container mx-auto py-16 text-2xl">
       <h2 class="text-center pb-3 select-none">Counter</h2>
-      <div class="mx-auto rounded-full bg-gradient-to-b from-indigo-500 via-purple-800 to-pink-500 w-32 h-32 flex items-center justify-center text-white font-bold text-3xl drop-shadow-md select-none [text-shadow:0_4px_8px_rgba(0,0,0,0.33)]">
+      <div class="mx-auto rounded-full bg-gradient-to-b from-indigo-500 via-purple-800 to-pink-500 w-32 h-32 flex items-center justify-center text-white font-bold text-3xl drop-shadow-md select-none [text-shadow:0_2px_4px_rgba(0,0,0,.67)] border-2  border border-indigo-400">
         <%= :gleam_app.get_counter_value(@gleam_store) %>
       </div>
       <div class="flex gap-2 justify-center pt-4">
-        <button href="#" phx-click="decrement" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner">
+        <button href="#" phx-click="decrement" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner border border-indigo-600">
           -
         </button>
-        <button href="#" phx-click="increment" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner">
+        <button href="#" phx-click="increment" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner border border-indigo-600">
           +
         </button>
       </div>
