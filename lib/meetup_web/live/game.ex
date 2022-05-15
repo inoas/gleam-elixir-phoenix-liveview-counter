c9cdc9a67a25a934b91c3a91357ea90b0ba8d678
inoas <mail@inoas.com>
2022-05-15 03:10:53 +0200

    guard string to atom operation

--- lib/meetup_web/live/game.ex
+++ lib/meetup_web/live/game.ex
@@ -6,25 +6,29 @@ defmodule MeetupWeb.Game do
   end
 
   def handle_event(action, _data, socket) do
-    gleam_store = :gleam_app.update(socket.assigns.gleam_store, String.to_atom(action))
-    {:noreply, assign(socket, gleam_store: gleam_store)}
+    actions = Enum.map(:gleam_app.actions(), &Atom.to_string(&1))
+
+    if action in actions do
+      gleam_store = :gleam_app.update(socket.assigns.gleam_store, String.to_atom(action))
+      {:noreply, assign(socket, gleam_store: gleam_store)}
+    end
   end
 
   def render(assigns) do
     ~H"""
     <div class="container mx-auto py-16 text-2xl">
-    	<h2 class="text-center pb-3 select-none">Counter</h2>
-    	<div class="mx-auto rounded-full bg-gradient-to-b from-indigo-500 via-purple-800 to-pink-500 w-32 h-32 flex items-center justify-center text-white font-bold text-3xl drop-shadow-md select-none [text-shadow:0_4px_8px_rgba(0,0,0,0.33)]">
-    		<%= :gleam_app.get_counter_value(@gleam_store) %>
-    	</div>
-    	<div class="flex gap-2 justify-center pt-4">
-    		<button href="#" phx-click="decrement" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner">
-    			-
-    		</button>
-    		<button href="#" phx-click="increment" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner">
-    			+
-    		</button>
-    	</div>
+      <h2 class="text-center pb-3 select-none">Counter</h2>
+      <div class="mx-auto rounded-full bg-gradient-to-b from-indigo-500 via-purple-800 to-pink-500 w-32 h-32 flex items-center justify-center text-white font-bold text-3xl drop-shadow-md select-none [text-shadow:0_4px_8px_rgba(0,0,0,0.33)]">
+        <%= :gleam_app.get_counter_value(@gleam_store) %>
+      </div>
+      <div class="flex gap-2 justify-center pt-4">
+        <button href="#" phx-click="decrement" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner">
+          -
+        </button>
+        <button href="#" phx-click="increment" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner">
+          +
+        </button>
+      </div>
     </div>
     """
   end
