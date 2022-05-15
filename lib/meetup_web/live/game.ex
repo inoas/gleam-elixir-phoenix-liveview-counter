b4391d955f1d16f425739affa48d35832b74300d
inoas <mail@inoas.com>
2022-05-15 02:15:22 +0200

    wip

--- lib/meetup_web/live/game.ex
+++ lib/meetup_web/live/game.ex
@@ -2,11 +2,13 @@ defmodule MeetupWeb.Game do
   use Phoenix.LiveView, layout: {MeetupWeb.LayoutView, "live.html"}
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, gleam_store: :gleam_game.create_store())}
+    {:ok, assign(socket, gleam_store: :gleam_game.create_store(0))}
   end
 
   def handle_event(action, _data, socket) do
-    gleam_store = socket.assigns.gleam_store |> :gleam_game.update(action |> String.to_atom())
+    IO.inspect({:elixir_before, socket.assigns.gleam_store})
+    gleam_store = :gleam_game.update(socket.assigns.gleam_store, String.to_atom(action))
+    IO.inspect({:elixir_after, gleam_store})
     {:noreply, assign(socket, gleam_store: gleam_store)}
   end
 
@@ -15,7 +17,7 @@ defmodule MeetupWeb.Game do
     <div class="container mx-auto py-16 text-2xl">
     	<h2 class="text-center pb-3">Counter</h2>
     	<div class="mx-auto rounded-full bg-gradient-to-b from-indigo-500 via-purple-800 to-pink-500 w-32 h-32 flex items-center justify-center text-white font-bold text-3xl drop-shadow-md select-none [text-shadow:0_4px_8px_rgba(0,0,0,0.33)]">
-    		<%= @gleam_store %>
+    		<%= @gleam_store |> elem(2) %>
     	</div>
     	<div class="flex gap-2 justify-center pt-4">
     		<button href="#" phx-click="decrement" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner">
--- src/gleam_game.gleam
+++ src/gleam_game.gleam
@@ -1,5 +1,11 @@
-pub fn create_store() -> Int {
-  0
+import gleam/io
+
+pub opaque type Counter {
+  Counter(step: Int, value: Int)
+}
+
+pub fn create_store(step: Int) -> Counter {
+  Counter(step: step, value: 0)
 }
 
 pub type Action {
@@ -11,9 +17,27 @@ pub fn actions() -> List(Action) {
   [Increment, Decrement]
 }
 
-pub fn update(store: Int, message: Action) -> Int {
+pub fn update(counter: Counter, message: Action) -> Counter {
   case message {
-    Increment -> store + 1
-    Decrement -> store - 1
+    Increment -> {
+      io.debug(#("gleam before inc", counter))
+      io.debug(#("gleam before inc step", counter.step))
+      io.debug(#("gleam before inc value", counter.value))
+      let counter = Counter(..counter, value: counter.value + counter.step)
+      io.debug(#("gleam after inc step", counter.step))
+      io.debug(#("gleam after inc value", counter.value))
+      io.debug(#("gleam after inc", counter))
+      counter
+    }
+    Decrement -> {
+      io.debug(#("gleam before inc", counter))
+      io.debug(#("gleam before inc step", counter.step))
+      io.debug(#("gleam before inc value", counter.value))
+      let counter = Counter(..counter, value: counter.value - counter.step)
+      io.debug(#("gleam after inc step", counter.step))
+      io.debug(#("gleam after inc value", counter.value))
+      io.debug(#("gleam after inc", counter))
+      counter
+    }
   }
 }
