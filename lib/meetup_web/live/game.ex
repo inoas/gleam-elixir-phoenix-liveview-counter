88f6c612e26cb013279acdb82a992c7eacc508a0
inoas <mail@inoas.com>
2022-05-15 14:02:09 +0200

    styling

--- lib/meetup_web/live/game.ex
+++ lib/meetup_web/live/game.ex
@@ -50,7 +50,7 @@ defmodule MeetupWeb.Game do
     ~H"""
     <div class="container mx-auto py-16 pb-24 text-2xl">
       <h2 class="text-center pb-3 select-none [text-shadow:0_2px_3px_rgba(0,0,0,.2)] font-medium">Counter</h2>
-      <div class="mx-auto rounded-full bg-gradient-to-b from-indigo-500 via-purple-800 to-pink-500 w-32 h-32 flex items-center justify-center text-white font-bold text-3xl drop-shadow-md select-none [text-shadow:0_2px_4px_rgba(0,0,0,.67)] border-2 border border-indigo-400">
+      <div class="mx-auto rounded-full bg-gradient-to-b from-indigo-500 via-purple-800 to-pink-500 w-44 h-44 flex items-center justify-center text-white font-bold text-3xl drop-shadow-md select-none [text-shadow:0_2px_4px_rgba(0,0,0,.67)] border-2 border border-indigo-400">
         <%= :gleam_app.get_counter_value(@gleam_store) %>
       </div>
       <div class="flex gap-2 justify-center pt-4">
@@ -70,7 +70,7 @@ defmodule MeetupWeb.Game do
     					caret-color: transparent;
             }
           </style>
-          <input name="step" value={:gleam_app.get_counter_step(@gleam_store)} phx-debounce="blur" type="number" min="1" max="5" step="1" inputmode="numeric" pattern="[1-9][0-9]" autofocus="autofocus" class="block rounded w-14 h-14 font-xl border border-indigo-600 text-center focus:outline-none focus:ring focus:ring-violet-300"/>
+          <input name="step" value={:gleam_app.get_counter_step(@gleam_store)} phx-debounce="blur" type="number" min="1" max="99" step="1" inputmode="numeric" pattern="[1-9][0-9]" autofocus="autofocus" class="block rounded w-14 h-14 font-xl border border-indigo-600 text-center focus:outline-none focus:ring focus:ring-violet-300"/>
         </form>
         <button phx-click="increment" class="block bg-indigo-500 text-white rounded w-14 h-14 font-xl hover:bg-indigo-700 drop-shadow select-none shadow hover:shadow-none hover:shadow-inner border border-indigo-600">
           +
