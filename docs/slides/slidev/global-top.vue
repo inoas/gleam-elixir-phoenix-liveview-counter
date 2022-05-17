b9f99d4f15b10e105d3d7ee608fddb5a8f1813ef
inoas <mail@inoas.com>
2022-05-18 00:19:17 +0200

    add one more playground link, fix footer, reexport

--- docs/slides/slidev/global-top.vue
+++ docs/slides/slidev/global-top.vue
@@ -1,4 +1,4 @@
 <!-- global-top.vue -->
 <template>
-  <footer class="absolute bottom-0 left-0 right-0 p-2 text-center text-xs">Gleam on Beam: Elixir's safe escape hatch / 2022-05-18 / Elixir Meetup Rhein Main</footer>
+  <footer class="absolute bottom-0 left-0 right-0 p-2 text-center text-xs">Gleam on Beam: Elixir's safe escape hatch / by <a href="https://github.com/inoas" target="_blank" class="underline">inoas</a> / 2022-05-18 / Elixir Meetup Rhein Main</footer>
 </template>
